619c1b7cbc728ac3d95da557974d259b642d2296
ansonmau <ansonmau172@gmail.com>
2026-05-13 11:59:21 -0400

    build changes
    feat: new icon

--- build/win/build.bat
+++ build/win/build.bat
@@ -2,6 +2,6 @@ pyinstaller "./src/main.py" ^
 --name "Dan Checker" ^
 --onefile ^
 --windowed ^
---icon="./build/icons/receipt-bordered.ico" ^
+--icon="./build/icons/receipt-new.ico" ^
 --distpath="bin/" ^
 --log-level INFO
