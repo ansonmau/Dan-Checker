527926013c62c74657266c1f6ca64c2a02cf9d56
ansonmau <ansonmau172@gmail.com>
2026-05-13 12:13:42 -0400

    testing with win build bat

--- build/win/build.bat
+++ build/win/build.bat
@@ -1,7 +1,17 @@
-pyinstaller "./src/main.py" ^
---name "Dan Checker" ^
+SET "root=C:/dev/dan-checker"
+SET "virt=Z:/"
+SET "name=Dan Checker"
+
+cd %root%
+:: ensure newest commit
+git pull
+:: pyinstaller magic
+pyinstaller "%root%/src/main.py" ^
+--name "%name%" ^
 --onefile ^
 --windowed ^
 --icon="./build/icons/receipt-new.ico" ^
 --distpath="bin/" ^
 --log-level INFO
+:: move to virtiofs share
+COPY "%root%/bin/%name%.exe" %virt%
